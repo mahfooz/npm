4a67e90e0bf6c59c1eaa99c82affb9ca1f1763b4
mahfooz <mahfooz.iiitian@gmail.com>
2020-12-22 18:02:54 +0530

    creating npm-git

---
+++ npm-git.sh
@@ -0,0 +1,18 @@
+#Step 1: Create a directory for your new package.
+mkdir packagename
+cd packagename
+
+#Step 2: Log on to Github and create a new repository for the package.
+#Initialize the git repository from npm and connect it to your GitHub Repo.
+
+git init  
+git remote add origin https://github.com/mahfooz/npm.git  
+
+#Step 3: Create package.json file.
+npm init
+
+#Step 4: Create index.js file
+
+#Step 5: Github creates README.md by default while creating the repository. Pull this file into npm package.
+
+
